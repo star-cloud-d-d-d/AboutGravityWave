de7101dd06049d633762fab1b3d4a50016471d47
star-cloud-d-d-d <hsinyun1111@icloud.com>
2025-04-04 16:09:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# 路由：首頁
+#路由:首頁
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# 路由：關於頁面
+#路由:補充說明
 @app.route('/some')
 def about():
     return render_template('some.html')
 
-# 路由：聯繫頁面
+#路由:參考資料
 @app.route('/ref')
 def contact():
     return render_template('ref.html')
